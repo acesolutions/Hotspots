5277c6c913b7f2760d1ee6394cbc92e5657d4f52
Jason Marden <themard@themard-XPS-L701X>
2013-04-24 22:52:38 +0100

    Added initial side navigation for application using wnielson's extension plugin

--- hotspots/app/view/Main.js
+++ hotspots/app/view/Main.js
@@ -206,10 +206,30 @@ Ext.define('hotshots.view.Main', {
             },{
                 xtype: 'container',
                 scrollable: 'vertical',
-                style: 'margin: auto !important; text-align: center;',
+                style: 'margin: auto !important;',
                 maskOnOpen: true,
                 items: [{
-                    
+                    html: '<h1><b>The Goal</b></h1>'+
+                    '<br>'+
+                    '<div style="text-align:left;">'+
+                    '<p>The goal of the application is simple Hotspot will allow all users to search for a gig or event by artist or event in which they can then find a gig they are interested in, once the specific event has been selected Hotspot aims to retrieve you all the places near the event which could be of interest. Being a fellow gig enthusiast, heres a number of places which could be of interest: Hotels, bars, clubs and maybe a restaurant.</p>'+
+                    '<br>'+
+                    '<p>More planning can equal a better experience, allowing you to relax and get you in the gigging mood without the hassle of searching several sources for places to see.</p>'+
+                    "<br>"+
+                    '<h2><b>Breakdown of features</b></h2>'+
+                    '<br>'+
+                    '<ul>'+
+                    '<li>Search for upcoming gigs to an artist or search for a specific event</li>'+
+                    '<br>'+
+                    '<li>Using filters choose some location categories, with this a map can be created with the locations plotted</li>'+
+                    '<br>'+
+                    '<li>Shortlist places of interest</li>'+
+                    '<br>'+
+                    '<li>Create a plan of places to see or places to stay</li>'+
+                    '<br>'+
+                    '<li>Add your event to a calendar</li>'+
+                    '</ul>'+
+                    '</div>'
                 }]
             }]
         }]
